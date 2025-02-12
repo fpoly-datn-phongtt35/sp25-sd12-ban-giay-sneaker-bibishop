ddd8b53ebde60028f373499450a90f23291a3293
Dung0123 <ledung2632003.com>
2025-02-12 19:15:41 +0700

    no

--- BibiStore - M/src/main/java/com/poly/BibiStore/repository/impl/HoaDonChiTietRepository.java
+++
@@ -1,8 +0,0 @@
-package com.poly.BibiStore.repository.impl;
-
-import com.poly.BibiStore.entity.HoaDonChiTiet;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HoaDonChiTietRepository extends CrudRepository<HoaDonChiTiet, Integer> {
-
-}
