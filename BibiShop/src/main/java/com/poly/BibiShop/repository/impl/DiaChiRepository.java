79d580d0f68b0a44100843a6935c90172334997d
Dung0123 <ledung2632003.com>
2025-02-12 20:36:02 +0700

    xoa toan bo noi dung

--- BibiShop/src/main/java/com/poly/BibiShop/repository/impl/DiaChiRepository.java
+++
@@ -1,8 +0,0 @@
-package com.poly.BibiStore.repository.impl;
-
-import com.poly.BibiStore.entity.DiaChi;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DiaChiRepository  extends CrudRepository<DiaChi, Integer> {
-
-}
